e7dc7edc0990c580613049668b4c775b6face4e4
Sara Fernandes <up201405955@fe.up.pt>
2020-01-03 16:34:33 +0000

    correlation clustering not working

--- hierarchicalClustering.py
+++ hierarchicalClustering.py
@@ -33,37 +33,6 @@ while n <= numFiles:
                 correlations[string][j] = value
             j += 1
         i += 1
-
-    cluster_th = 4
-
-    X = correlations.values
-    d = sch.distance.pdist(X)
-    L = sch.linkage(d, method='complete')
-    ind = sch.fcluster(L, 0.5*d.max(), 'distance')
-
-    columns = [data.columns.tolist()[i] for i in list(numpy.argsort(ind))]
-    data = data.reindex_axis(columns, axis=1)
-
-    unique, counts = numpy.unique(ind, return_counts=True)
-    counts = dict(zip(unique, counts))
-
-    i = 0
-    j = 0
-    columns = []
-    for cluster_l1 in set(sorted(ind)):
-        j += counts[cluster_l1]
-        sub = data[data.columns.values[i:j]]
-        if counts[cluster_l1]>cluster_th:        
-            X = sub.corr().values
-            d = sch.distance.pdist(X)
-            L = sch.linkage(d, method='complete')
-            ind = sch.fcluster(L, 0.5*d.max(), 'distance')
-            col = [sub.columns.tolist()[i] for i in list((numpy.argsort(ind)))]
-            sub = sub.reindex_axis(col, axis=1)
-        cols = sub.columns.tolist()
-        columns.extend(cols)
-        i = j
-    data = data.reindex_axis(columns, axis=1)
     
     corr = correlations
 
