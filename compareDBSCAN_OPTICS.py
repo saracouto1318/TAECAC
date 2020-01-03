76a382407ed517d7e5f1a88e0de35bde60f953d4
Sara Fernandes <up201405955@fe.up.pt>
2020-01-03 16:16:47 +0000

    Update compareDBSCAN_OPTICS.py

--- compareDBSCAN_OPTICS.py
+++ compareDBSCAN_OPTICS.py
@@ -13,7 +13,6 @@ n = 1
 while n <= numFiles:
     names = ['Code Smell', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13', 'S14', 'S15', 'S16', 'S17', 'S18', 'S19', 'S20', 'S21', 'S22', 'S23', 'S24', 'S25', 'S26', 'S27', 'S28', 'S29', 'S30', 'S31', 'S32', 'S33', 'S34', 'S35']
     fileName = DIR + "/Smells" + str(n) + ".csv"
-    print(fileName)
     data = pandas.read_csv(fileName, header=0, names=names)
     data.drop(['Code Smell'], axis = 1, inplace = True)
 
