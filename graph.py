73676b49fbb49d185b334e7a02eecb92def0f8b4
Sara Fernandes <up201405955@fe.up.pt>
2020-01-06 16:56:36 +0000

    Update graph.py

--- graph.py
+++ graph.py
@@ -63,7 +63,7 @@ for point2 in range(1, n):
 
     X[point2,:point2] = sympy.solve(expressions, list(X[point2,:point2]))[1]
                
-with open('Smells2.csv', 'wb') as csvfile:
+with open('Smells-Distances.csv', 'wb') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     filewriter.writerow(['Code Smell', 'S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13', 'S14', 'S15', 'S16', 'S17', 'S18', 'S19', 'S20', 'S21', 'S22', 'S23', 'S24', 'S25', 'S26', 'S27', 'S28', 'S29', 'S30', 'S31', 'S32', 'S33', 'S34', 'S35'])
@@ -74,8 +74,3 @@ with open('Smells2.csv', 'wb') as csvfile:
         filewriter.writerow([string, array[i], array[i+1], array[i+2], array[i+3], array[i+4], array[i+5], array[i+6], array[i+7], array[i+8], array[i+9], array[i+10], array[i+11], array[i+12], array[i+13], array[i+14], array[i+15], array[i+16], array[i+17], array[i+18], array[i+19], array[i+20], array[i+21], array[i+22], array[i+23], array[i+24], array[i+25], array[i+26], array[i+27], array[i+28], array[i+29], array[i+30], array[i+31], array[i+32], array[i+33], array[i+34]])
         i += 35
         line += 1
- 
-col_names = ['Smells','S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'S11', 'S12', 'S13', 'S14', 'S15', 'S16', 'S17', 'S18', 'S19', 'S20', 'S21', 'S22', 'S23', 'S24', 'S25', 'S26', 'S27', 'S28', 'S29', 'S30', 'S31', 'S32', 'S33', 'S34', 'S35']        
-pima = pd.read_csv("Smells2.csv", header=0, names=col_names)
-pima.head()       
-print(pima)
